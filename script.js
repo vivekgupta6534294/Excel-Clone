8a8cbee5a629ad696473b7141e0b5d7d8b8cf0f0
vivek gupta <vivekgupta6534294@gmail.com>
2022-05-26 16:59:23 +0530

    Cells Section Ui

--- script.js
+++ script.js
@@ -1,17 +1,36 @@
-let cellsContent=document.querySelector(".cells-content");
+let cellsContentDiv=document.querySelector(".cells-content");
 
 
 
 function initCells(){
-    let cell="";
+    //Blank Wala Space
+    let cellsContent="<div class='top-left-cell'></div>";
+    
+    //top row
+    cellsContent+="<div class='top-row'>";
+   //for colomn
+    for(let i=0;i<26;i++){
+        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
+    }
+    cellsContent+="</div>";
+    // left col
+    cellsContent+="<div class='left-col'>";
     for(let i=0;i<100;i++){
-        cell+="<div class='row'>";
+        cellsContent+=`<div class='left-col-cell'>${i+1}</div>`
+    
+    }
+    cellsContent+="</div>";
+
+    cellsContent+="<div class='cells'>";
+    for(let i=0;i<100;i++){
+        cellsContent+="<div class='row'>";
         // Coloumn
         for(let j=0;j<26;j++){
-            cell+="<div class='cell'>Cell</div>";
+            cellsContent+="<div class='cell' contentEditable>Cell</div>";
         }
-        cell+="</div>"
+        cellsContent+="</div>"
     }
-    cellsContent.innerHTML=cell;
+    cellsContent+="</div>";
+    cellsContentDiv.innerHTML=cellsContent;
 }
 initCells();
