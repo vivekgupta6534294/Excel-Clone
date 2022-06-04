855e0d83ad8df72074023f3af941212c1a8cda9b
vivek gupta <vivekgupta6534294@gmail.com>
2022-06-04 12:53:29 +0530

    Address display

--- init.js
+++ init.js
@@ -26,7 +26,7 @@ function initCells() {
         cellsContent += "<div class='row'>";
         // Coloumn
         for (let j = 0; j < 26; j++) {
-            cellsContent += "<div class='cell' contentEditable></div>";
+            cellsContent += `<div class = 'cell' rowid='${i}' colid='${j}' contentEditable></div>`
         }
         cellsContent += "</div>"
     }
