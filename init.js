746419b0a6d18d96903be24507b3f752cdab36ab
vivek gupta <vivekgupta6534294@gmail.com>
2022-06-04 12:15:36 +0530

    topRow, topColumn, topLeftRow Set

---
+++ init.js
@@ -0,0 +1,36 @@
+let cellsContentDiv = document.querySelector(".cells-content");
+
+
+
+function initCells() {
+    //Blank Wala Space
+    let cellsContent = "<div class='top-left-cell'></div>";
+
+    //top row
+    cellsContent += "<div class='top-row'>";
+    //for colomn
+    for (let i = 0; i < 26; i++) {
+        cellsContent += `<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
+    }
+    cellsContent += "</div>";
+    // left col
+    cellsContent += "<div class='left-col'>";
+    for (let i = 0; i < 100; i++) {
+        cellsContent += `<div class='left-col-cell'>${i+1}</div>`
+
+    }
+    cellsContent += "</div>";
+
+    cellsContent += "<div class='cells'>";
+    for (let i = 0; i < 100; i++) {
+        cellsContent += "<div class='row'>";
+        // Coloumn
+        for (let j = 0; j < 26; j++) {
+            cellsContent += "<div class='cell' contentEditable></div>";
+        }
+        cellsContent += "</div>"
+    }
+    cellsContent += "</div>";
+    cellsContentDiv.innerHTML = cellsContent;
+}
+initCells();
