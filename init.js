d7bda61a2d352e8157ce95ef05c92cf0a5c378c6
vivek gupta <vivekgupta6534294@gmail.com>
2022-07-06 12:51:43 +0530

    Sheet Logic Implemented

--- init.js
+++ init.js
@@ -1,55 +1,59 @@
 let cellsContentDiv = document.querySelector(".cells-content");
 
 
-function initCells() {
+function initCells(){
     let cellsContent = "<div class='top-left-cell'></div>";
     //top-row
-    cellsContent += "<div class='top-row'>"
-    for (let i = 0; i < 26; i++) {
-        cellsContent += `<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
+    cellsContent+="<div class='top-row'>"
+    for(let i=0;i<26;i++){
+        cellsContent+=`<div class='top-row-cell'>${String.fromCharCode(65+i)}</div>`;
     }
-    cellsContent += "</div>"
+    cellsContent+="</div>"
 
     //left-col
-    cellsContent += "<div class='left-col'>"
-    for (let i = 0; i < 100; i++) {
-        cellsContent += `<div class='left-col-cell'>${i+1}</div>`
+    cellsContent+="<div class='left-col'>"
+    for(let i=0;i<100;i++){
+        cellsContent+=`<div class='left-col-cell'>${i+1}</div>`
     }
-    cellsContent += "</div>"
-    cellsContent += "<div class='cells'>"
-    for (let i = 0; i < 100; i++) {
-        cellsContent += "<div class = 'row'>";
+    cellsContent+="</div>"
+    cellsContent+="<div class='cells'>"
+    for(let i=0;i<100;i++){
+        cellsContent+="<div class = 'row'>";
         //column
-        for (let j = 0; j < 26; j++) {
-            cellsContent += `<div class = 'cell' rowid='${i}' colid='${j}' contentEditable></div>`
+        for(let j=0;j<26;j++){
+            cellsContent+=`<div class = 'cell' rowid='${i}' colid='${j}' contentEditable></div>`
         }
-        cellsContent += "</div>"
+        cellsContent+="</div>"
     }
-    cellsContent += "</div>"
+    cellsContent+="</div>"
     cellsContentDiv.innerHTML = cellsContent;
 }
 
 initCells();
 
-let db;
+let sheetsDB = [];
+let db; //[[{},{},{}......{}],[[{},{},{}......{}],[[{},{},{}......{}]]
 
-function initDb() {
-    db = [];
-    for (let i = 0; i < 100; i++) {
+function initDb(){
+    let newSheetDB = [];
+    for(let i=0;i<100;i++){
         let row = [];
-        for (let j = 0; j < 26; j++) {
-            let name = String.fromCharCode(65 + j) + (i + 1) + "";
+        for(let j=0;j<26;j++){
+            let name = String.fromCharCode(65+j)+(i+1)+"";
             let cellObject = {
-                name: name,
-                value: "",
-                formula: "",
+                name:name,
+                value:"",
+                formula:"",
                 children:[],
                 parent:[]
             }
             row.push(cellObject);
         }
-        db.push(row);
+        newSheetDB.push(row);
     }
+    db = newSheetDB;
+    sheetsDB.push(newSheetDB);
+    console.log(sheetsDB);
 }
 initDb();
 
